666b41fb0a94033d880f2c274057c724d570832f
minghe <minghe36@126.com>
2013-01-29 09:49:52 +0800

    fix gallery #128

--- gallery/form/1.4/uploader/queue.js
+++ gallery/form/1.4/uploader/queue.js
@@ -116,8 +116,19 @@ KISSY.add('gallery/form/1.4/uploader/queue', function (S, Node, Base) {
             //如果存在多个文件，需要批量添加文件
             if (files.length > 0) {
                 fileData=[];
-                S.each(files,function(file){
-                    fileData.push(self._addFile(file));
+                var len = self.get('files').length;
+                var hasMax = S.isNumber(uploader.get('max'));
+                S.each(files,function(file, index){
+                    if(!hasMax){
+                        fileData.push(self._addFile(file));
+                    }else{
+                        //增加是否超过判断
+                        //#128 https://github.com/kissyteam/kissy-gallery/issues/128 by 翰文
+                        var max = self.get('max');
+                        if (max >= len + index + 1) {
+                            fileData.push(self._addFile(file));
+                        }
+                    }
                 });
             } else {
                 fileData = self._addFile(files);
